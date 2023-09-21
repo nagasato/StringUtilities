029d0407e0c706535114827e322a80f940172fc4
nagasato <nagasatox@gmail.com>
2023-09-21 21:57:55 +0900

    ByteStringExtension

---
+++ StringUtilities/ByteStringExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringUtilities
+{
+    /// <summary>
+    /// ByteStringExtensions
+    /// </summary>
+    public static class ByteStringExtensions
+    {
+        /// <summary>
+        /// valueをbyteLengthずつに区切って列挙する
+        /// (多バイト文字の途中で区切られないように考慮)
+        /// <param name="value"></param>
+        /// <param name="byteLength"></param>
+        /// <param name="encoding">省略した場合はShift_JIS</param>
+        /// <returns></returns>
+        public static IEnumerable<string> FixedByteSplit(this string value, int byteLength, Encoding encoding)
+        {
+            if (value == null) { throw new ArgumentNullException(); }
+
+            var s = value;
+            while (!string.IsNullOrEmpty(s))
+            {
+                // lengthバイトまでの文字をcharとして列挙->string生成
+                var chars = s.TakeWhile((c, i) => encoding.GetByteCount(s.Substring(0, i + 1)) <= byteLength).ToArray();
+                var cutout = new string(chars);
+                yield return cutout;
+
+                s = s.Replace(cutout, string.Empty); // 切り出した分を詰める
+            }
+        }
+
+        /// <summary>
+        /// valueをbyteLengthになるようにPadCharで右埋めにする
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="byteLength">byte数</param>
+        /// <param name="encoding"></param>
+        /// <param name="padChar">省略した場合は空白</param>
+        /// <returns></returns>
+        public static string PadByteRight(this string value, int byteLength, Encoding encoding, char padChar = ' ')
+        {
+            if (value == null) { throw new ArgumentNullException(); }
+
+            encoding ??= Encoding.GetEncoding("Shift_JIS");
+            var valueLen = encoding.GetByteCount(value);
+            if (valueLen >= byteLength) { return value; }
+
+            var pad = new string(padChar, byteLength - valueLen);
+            return value + pad;
+        }
+
+        /// <summary>
+        /// valueをbyteLengthになるようにPadCharで左埋めにする
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="byteLength">byte数</param>
+        /// <param name="encoding"></param>
+        /// <param name="padChar">省略した場合は空白</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string PadByteLeft(this string value, int byteLength, Encoding encoding, char padChar = ' ')
+        {
+            if (value == null) { throw new ArgumentNullException(); }
+
+            var valueLen = encoding.GetByteCount(value);
+            if (valueLen >= byteLength) { return value; }
+
+            var pad = new string(padChar, byteLength - valueLen);
+            return pad + value;
+        }
+    }
+}
---
+++ StringUtilitiesTests/ByteStringExtensionsTest.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringUtilities;
+
+namespace StringUtilitiesTests
+{
+    [TestClass]
+    public class ByteStringExtensionsTest
+    {
+        public ByteStringExtensionsTest()
+        {
+#if NET
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+#endif
+        }
+
+        [TestMethod]
+        public void FixedByteSplit()
+        {
+            var source1 = "1壱45弐890";
+            var expected1 = new string[]
+            {
+                "1",
+                "壱",
+                "45",
+                "弐",
+                "89",
+                "0"
+            };
+
+            var encoding = Encoding.GetEncoding("Shift_JIS");
+            var ret1 = source1.FixedByteSplit(2, encoding).ToArray(); // Shift_JIS
+            for(var i = 0; i < expected1.Length; i++)
+            {
+                Assert.AreEqual(expected1[i], ret1[i]);
+            }
+        }
+
+        [TestMethod]
+        public void PadByteLeftTest()
+        {
+            // TODO: Dictionaryに書き換える ( [key]expected -> [value]ret )
+            var source1 = "愛";
+            var expected1 = new string[]
+            {
+                "愛",    // byteLength=0
+                "愛",    // byteLength=1
+                "愛",    // byteLength=2
+                " 愛",   // byteLength=3
+                "  愛",  // byteLength=4
+            };
+
+            var encoding = Encoding.GetEncoding("Shift_JIS");
+            var ret1 = Enumerable.Range(0, expected1.Length).Select(i => source1.PadByteLeft(i, encoding)).ToArray();
+            for (var i = 0; i < expected1.Length; i++)
+            {
+                Assert.AreEqual(expected1[i], ret1[i]);
+            }
+        }
+
+        [TestMethod]
+        public void PadByteRightTest()
+        {
+            var source1 = "愛";
+            var expected1 = new string[]
+            {
+                "愛",    // byteLength=0
+                "愛",    // byteLength=1
+                "愛",    // byteLength=2
+                "愛 ",   // byteLength=3
+                "愛  ",  // byteLength=4
+            };
+
+            var encoding = Encoding.GetEncoding("Shift_JIS");
+            var ret1 = Enumerable.Range(0, expected1.Length).Select(i => source1.PadByteRight(i, encoding)).ToArray();
+            for (var i = 0; i < expected1.Length; i++)
+            {
+                Assert.AreEqual(expected1[i], ret1[i]);
+            }
+        }
+    }
+}
